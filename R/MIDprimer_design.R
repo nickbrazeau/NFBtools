920a1bd34802e3655eea2705abe4f71059987096
Nicholas Brazeau <nbrazeau@med.unc.edu>
2018-08-29 17:17:52 -0400

    midprimer design overhaul

--- R/MIDprimer_design.R
+++ R/MIDprimer_design.R
@@ -21,7 +21,8 @@ MIDmaker <- function(length, homopolymercutoff){
 #' @title MID Primer Design
 #'
 #' @author Nick Brazeau
-#' @description TODO
+#' @description This will produces MIDs
+#' @details This is a brute-force approach with recursion as the number of new MIDs proposed is based on "how close we are to solving the problem" with the current sit of MIDs
 #'
 #' @param design_fasta is expected to have the forward, reverse, and target gene in it. Must have `forward` and `reverse` in the contig names for the forward and reverse primers
 #' @param MID2targetmismatchesAllowed is the number of base-pair mismatches allowed in the MID before it would bind to ANY place on the target gene
@@ -30,7 +31,7 @@ MIDmaker <- function(length, homopolymercutoff){
 MIDPrimerFinder <- function(design_fasta = NULL,
                             MID2MIDmatchesAllowed = NULL,
                             MIDlength = NULL,
-                            MIDhomopolymercutoff = NULL,
+                            MIDhomopolymerallowance = NULL,
                             MID2targetmismatchesAllowed = NULL,
                             MIDnum = NULL
                             ){
@@ -45,7 +46,7 @@ MIDPrimerFinder <- function(design_fasta = NULL,
   # error handling
 
   hits <- lapply(list(fw, rv), function(x) {
-    vmatchPattern(as.character(x), tg) # coercion here is fine since dnastrinset of length 1 ... becomes a named character vector
+    Biostrings::vmatchPattern(as.character(x), tg) # coercion here is fine since dnastrinset of length 1 ... becomes a named character vector
   })
 
 
@@ -57,11 +58,11 @@ MIDPrimerFinder <- function(design_fasta = NULL,
     stop("Your primers have more than one binding site on your target")
   }
 
-  if( all(sapply(c(MID2MIDmatchesAllowed, MIDlength, MIDhomopolymercutoff, MID2targetmismatchesAllowed, MIDnum), class) != "numeric") ){
+  if( all(sapply(c(MID2MIDmatchesAllowed, MIDlength, MIDhomopolymerallowance, MID2targetmismatchesAllowed, MIDnum), class) != "numeric") ){
     stop("Your MID specifications all must be of class numeric")
   }
 
-  if( !all( c(MID2MIDmatchesAllowed, MIDlength, MIDhomopolymercutoff, MID2targetmismatchesAllowed, MIDnum) %in% sapply(c(MID2MIDmatchesAllowed, MIDlength, MIDhomopolymercutoff, MID2targetmismatchesAllowed, MIDnum), floor)) ){
+  if( !all( c(MID2MIDmatchesAllowed, MIDlength, MIDhomopolymerallowance, MID2targetmismatchesAllowed, MIDnum) %in% sapply(c(MID2MIDmatchesAllowed, MIDlength, MIDhomopolymerallowance, MID2targetmismatchesAllowed, MIDnum), floor)) ){
     stop(" Your MID specifications must be whole numbers (no decimals)")
   }
 
@@ -92,30 +93,40 @@ MIDPrimerFinder <- function(design_fasta = NULL,
 
   while( length(finalMIDs) < n ){
 
-    propMIDs <- replicate(n, MIDmaker(MIDlength, MIDhomopolymercutoff)) # init
+    propMIDs <- replicate(n, MIDmaker(MIDlength, MIDhomopolymerallowance)) # init
+    propMIDs <- c(propMIDs, currMIDs)
 
 
     while( any( TRUE %in% c(Biostrings::reverseComplement(Biostrings::DNAStringSet(propMIDs)) %in% Biostrings::DNAStringSet(propMIDs)) ) | # catch any reverse complements -- repropose
-           sum(sapply(propMIDs, function(x){Biostrings::vcountPattern(x, tg, max.mismatch = MID2targetmismatchesAllowed) })) > 0 ) { # catch any close matches to target
-
+           sum(sapply(propMIDs, function(x){Biostrings::vcountPattern(x, tg, max.mismatch = MID2targetmismatchesAllowed) })) > 0 | # catch any close matches to target
+           any( TRUE %in%  duplicated(propMIDs) )
+           ){
         # find culprits for rev comp
         revcount <- sum(Biostrings::reverseComplement(Biostrings::DNAStringSet(propMIDs)) %in% Biostrings::DNAStringSet(propMIDs))
         if(revcount > 0){
-          propMIDs[ Biostrings::reverseComplement(Biostrings::DNAStringSet(propMIDs)) %in% Biostrings::DNAStringSet(propMIDs) ] <- replicate(revcount, MIDmaker(MIDlength, MIDhomopolymercutoff))
+          propMIDs[ Biostrings::reverseComplement(Biostrings::DNAStringSet(propMIDs)) %in% Biostrings::DNAStringSet(propMIDs) ] <- replicate(revcount, MIDmaker(MIDlength, MIDhomopolymerallowance))
         }
 
         # find culprits for overlap targets
         ot_vect <- which( sapply(propMIDs, function(x){Biostrings::vcountPattern(x, tg, max.mismatch = MID2targetmismatchesAllowed) }) > 0 )
         if(length(ot_vect) > 0){
-        propMIDs[ot_vect] <- replicate(length(ot_vect), MIDmaker(MIDlength, MIDhomopolymercutoff))
+        propMIDs[ot_vect] <- replicate(length(ot_vect), MIDmaker(MIDlength, MIDhomopolymerallowance))
         }
-    }
+
+        # find culprits for duplicates
+        propMIDs[duplicated(propMIDs)] <-  replicate(sum(duplicated(propMIDs)), MIDmaker(MIDlength, MIDhomopolymerallowance))
+
+
+        # make sure to return a vector
+        propMIDs <- unlist(propMIDs) # if replicate has n=0, it returns a list class which will coerce our atomic vector into a list. Don't want this
+
+      }
 
 
 
 
-    if(int%%10 == 0){cat(paste0("Done with proposal iteration", int, "\n ------- \n"))}
-    propMIDs <- c(propMIDs, currMIDs)
+
+    if(int%%10 == 0){cat(paste0("Iteration  ", int, "\n ------- \n"))}
     # get pairs and pair.list
     pairs <- t(combn(propMIDs,m=2)) # choose 2
     pairs_list <- split(pairs, seq(nrow(pairs)))
@@ -133,7 +144,7 @@ MIDPrimerFinder <- function(design_fasta = NULL,
       # Standardize to length and then select those MIDs that have the best distances so far
       currMIDs <- sample(MIDret$primer, size = keep, prob = MIDret$Hdist/MIDlength)
       # update N for propsoal
-      n <- MIDsWanted - length(currMIDs)
+      n <- MIDnum - length(currMIDs)
 
       # housekeeping
       int <- int + 1
