37eab8c4d68ba161472fa1c4bb6d89feca2f0a59
Nicholas Brazeau <nbrazeau@med.unc.edu>
2018-09-05 18:41:03 -0400

    fix build

--- src/misc_prob.h
+++
@@ -1,4 +0,0 @@
-//------------------------------------------------
-// draw from negative binomial distribution with mean lambda and variance gamma*lambda (gamma must be >1)
-int rnbinom1(double lambda, double gamma);
-double meanC(NumericVector x);
